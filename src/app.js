47c31eb703b8970be5fec2e3be326230e46877ac
abdelrahman <abdelrahmannn202@gmail.com>
2026-04-28 03:08:55 +0300

    feat: add Express app with cors, json parser, auth/bill routes, swagger UI, and error handler

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
+import billRoutes from "./routes/billRoutes.js";
+import { serveSwagger, setupSwagger } from "./config/swagger.js";
+import AppError from "./utils/appError.js";
+
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/bills", billRoutes);
+app.use("/docs", serveSwagger, setupSwagger);
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "Done",
+    message: "Server is running",
+  });
+});
+
+app.use((req, res, next) => {
+  next(new AppError(404, `Cannot ${req.method} ${req.originalUrl}`));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message,
+  });
+});
+
+export default app;
