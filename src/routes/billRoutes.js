4a0b1e1c07627c5bd8090b9589c025c69a5005e6
abdelrahman <abdelrahmannn202@gmail.com>
2026-04-28 06:44:36 +0300

    feat: change bills by merchant route from /merchant/:merchantId to /merchant with query param

--- src/routes/billRoutes.js
+++ src/routes/billRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
-import { createBill, getBill } from "../controllers/bills/billsController.js";
+import {
+  createBill,
+  getBill,
+  getBillsByMerchantId,
+} from "../controllers/bills/billsController.js";
 
 const router = Router();
 
 router.post("/", asyncHandler(createBill));
+router.get("/merchant", asyncHandler(getBillsByMerchantId));
 router.get("/:id", asyncHandler(getBill));
 
 export default router;
