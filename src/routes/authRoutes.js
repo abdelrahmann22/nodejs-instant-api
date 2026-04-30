caea0f227fefc699d0c970c35cf9cb32863323c5
abdelrahman <abdelrahmannn202@gmail.com>
2026-04-30 08:49:15 +0300

    refactor: update authRoutes to import from split merchant/user controllers

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 import {
   merchantSignup,
   merchantLogin,
-  userSignup,
-  userLogin,
-} from "../controllers/auth/authController.js";
+  getOnboardingLink,
+} from "../controllers/auth/merchantController.js";
+import { userSignup, userLogin } from "../controllers/auth/userController.js";
 
 const router = Router();
 
 router.post("/merchant/signup", asyncHandler(merchantSignup));
 router.post("/merchant/login", asyncHandler(merchantLogin));
+router.post("/merchant/onboarding-link", protect, restrictTo("merchant"), asyncHandler(getOnboardingLink));
 router.post("/user/signup", asyncHandler(userSignup));
 router.post("/user/login", asyncHandler(userLogin));
 
