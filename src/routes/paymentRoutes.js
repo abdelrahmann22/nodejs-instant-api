ac9327a5db4e222984821422286c0a45f07c5d34
abdelrahman <abdelrahmannn202@gmail.com>
2026-05-01 09:22:36 +0300

    refactor: format payment routes + remove merchant-only restriction on GET /:bill_id

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,13 +1,33 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
 import { protect, restrictTo } from "../middlewares/authMiddleware.js";
-import { initiatePaymentController, getPaymentsByBillId, getPaymentsByUser, cancelPaymentController } from "../controllers/payments/paymentsController.js";
+import {
+  initiatePaymentController,
+  getPaymentsByBillId,
+  getPaymentsByUser,
+  cancelPaymentController,
+} from "../controllers/payments/paymentsController.js";
 
 const router = Router();
 
-router.post("/initiate", protect, restrictTo("user"), asyncHandler(initiatePaymentController));
-router.post("/:id/cancel", protect, restrictTo("user"), asyncHandler(cancelPaymentController));
-router.get("/user", protect, restrictTo("user"), asyncHandler(getPaymentsByUser));
-router.get("/:bill_id", protect, restrictTo("merchant"), asyncHandler(getPaymentsByBillId));
+router.post(
+  "/initiate",
+  protect,
+  restrictTo("user"),
+  asyncHandler(initiatePaymentController),
+);
+router.post(
+  "/:id/cancel",
+  protect,
+  restrictTo("user"),
+  asyncHandler(cancelPaymentController),
+);
+router.get(
+  "/user",
+  protect,
+  restrictTo("user"),
+  asyncHandler(getPaymentsByUser),
+);
+router.get("/:bill_id", protect, asyncHandler(getPaymentsByBillId));
 
 export default router;
