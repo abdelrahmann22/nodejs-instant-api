36ddf4bd1ee450fef4e35d77df86237dd2c48199
abdelrahman <abdelrahmannn202@gmail.com>
2026-05-01 08:54:44 +0300

    feat: add emitPaymentInitiated socket event

--- src/services/realtime/realtimeService.js
+++ src/services/realtime/realtimeService.js
@@ -2,6 +2,26 @@ import { getIO } from "../../config/socket.js";
 import * as billRepo from "../../repositories/billRepo.js";
 import * as authRepo from "../../repositories/authRepo.js";
 
+export const emitPaymentInitiated = async (billId, payment) => {
+  const io = getIO();
+
+  const bill = await billRepo.findBillById(billId);
+  const paid_amount = await billRepo.findSucceededAmountByBillId(billId);
+  const pending_amount = await billRepo.findPendingAmountByBillId(billId);
+  const remaining = parseFloat(bill.amount) - paid_amount - pending_amount;
+
+  const user = await authRepo.findUserById(payment.user_id);
+
+  io.to(`bill:${billId}`).emit("payment:initiated", {
+    pending_amount,
+    remaining,
+    contributor: {
+      name: user?.username || "Anonymous",
+      amount: parseFloat(payment.amount),
+    },
+  });
+};
+
 export const emitPaymentSucceeded = async (billId, payment) => {
   const io = getIO();
 
